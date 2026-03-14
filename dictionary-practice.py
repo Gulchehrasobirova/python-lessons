185f5f89ff8e800c3d57f9207f47b33bf2f2d736
Gulchehrasobirova <G39075492gmail.com>
2026-03-14 16:19:43 +0500

    python strings

---
+++ dictionary-practice.py
@@ -0,0 +1,19 @@
+Father = {}
+dad = {'name':'Bekzod', 'year':1990,'region':'Khorazm'}
+year = dad['year']
+region = dad['region']
+print(f"dad name {dad['name']}, {year}-year,{region}- born in the khorezm region")
+
+mother = {}
+mom = {'name':'Lobar', 'year':1991,'region':'Khorazm'}
+year = mom['year']
+year = mom['region']
+print(f"mom name {mom['name'].title()}, {year}-years, {year.title()} born in the khorezm region")
+
+
+foods = {
+    'father':'osh',
+    'mother':'manti',
+    'me':'somsa',
+    'brother':'shashlik'
+}
