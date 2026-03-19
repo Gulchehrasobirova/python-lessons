9666ab47734d52ca03a997e5689c1db9f714060b
Gulchehrasobirova <G39075492gmail.com>
2026-03-19 16:26:43 +0500

    Python-algorithms

---
+++ dict-elements-practice.py
@@ -0,0 +1,33 @@
+# Amaliyot
+# 1.
+python_dict = {
+    'boolean':'Mantiqiy qiymat',
+    'float':'onlik sonlar',
+    'for':'biror amalni qayta-qayta bajarish tsikli',
+    'if':'shartlarni teshkirish operatori',
+    'integer':'butun son',
+    'else':'aks holda operatori',
+    'dict':'lug\'at'
+}
+
+print("Pyton izohli lug'ati (alifbo tartibida):\n")
+for key,value in sorted (python_dict.items()):
+    print(f"{key.title()} - {value.title()}")
+# 2.
+davlatlar_poytaxtlar = {
+    'AQSH':'Washington.D.C',
+    'Italiya':'Rim',
+    'Malayziya':'Kuala',
+    "O'zbekiston":'Toshkent',
+}
+print("Dunyo davlatlari: ")
+for poytaxt in sorted(davlatlar_poytaxtlar.values()):
+    print(poytaxt)
+davlat = input("Davlat nomini kiriting: ").lower()
+
+
+
+
+
+
+
